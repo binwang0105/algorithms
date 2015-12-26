47c08a87ce2477976b0d6626df65ce08a8912844
unknown <381242448@qq.com>
2015-12-26 15:50:42 -0500

    add exercise 257

--- exercise_1.java
+++ exercise_1.java
@@ -1,22 +1,16 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
-    public ListNode reverseList(ListNode head) {
-       if(head == null || head.next == null)
-            return head;
-        ListNode nextNode = head.next;
-        ListNode newHead = reverseList(nextNode);
-        nextNode.next = head; 
-        head.next = null;
-        return newHead;
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                result[1] = i + 1;
+                result[0] = map.get(target-nums[i]);
+            }
+            else{
+                map.put(nums[i], i+1);
+            }
+        }
+        return result;
     }
 }
-
-1. Time Complexity: O(n)
-2. 需要判断head以及head.next是否存在
---
+++ exercise_257.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new ArrayList<String>();
+        if(root != null)
+            searchBT(root, "", list);
+        return list;
+    }
+    
+     public void searchBT(TreeNode root, String path, List<String> list){
+        if(root.left == null && root.right == null)
+            list.add(path + root.val);
+        if(root.left != null)
+            searchBT(root.left, path + root.val + "->", list);
+        if(root.right != null)
+            searchBT(root.right, path + root.val + "->", list);
+    }
+}
+
+/*
+* 1. 注意DFS的顺序，如果不为空就一直访问孩子，对于二叉树先左后右
+* 2. DFS本质在于递归，所以应该是一个函数调用另一个函数
+*
+*/
