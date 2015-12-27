3a0dd0473687baa98c5f249d2701a59bfa4b17f7
unknown <381242448@qq.com>
2015-12-27 15:41:46 -0500

    add exercise_226

---
+++ exercise_226.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        TreeNode temp = root;
+        if(root == null)
+            return root;
+        else{
+            temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        return root;
+    }
+}
+
+// exchange left child and right child
+// do recursion
