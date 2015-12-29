8e813807f1d49ed117a533b4835503696e689243
unknown <381242448@qq.com>
2015-12-29 01:07:02 -0500

    add exercise_70

---
+++ exercise_70.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if(n == 0||n == 1||n == 2)
+            return n;  //this is the base cases for DP
+        int[] mem = new int[n];    
+            mem[0] = 1;
+            mem[1] = 2;
+            for(int i = 2; i < n; i++){
+                mem[i] = mem[i-1] + mem[i-2];
+            }
+        return mem[n-1];
+    }
+}
