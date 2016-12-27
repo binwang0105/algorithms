d1d8c4a6b643fbc49a1d728aa79108717f1dd0b7
unknown <381242448@qq.com>
2016-12-26 19:48:09 -0500

    add 414

---
+++ exercise_414.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int thirdMax(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        Set set = new HashSet();
+        for(int n : nums){
+            if(set.add(n)){
+                pq.offer(n);
+                if(pq.size() > 3)
+                    pq.poll();
+            }
+        }
+        if(pq.size() == 2)
+            pq.poll();
+        return pq.peek();
+    }
+}
