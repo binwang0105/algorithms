713995dea3c502e472a7be19cedee9177b04cbd7
unknown <381242448@qq.com>
2016-12-28 18:46:24 -0500

    add 380

---
+++ exercise_380.java
@@ -0,0 +1,42 @@
+public class RandomizedSet {
+
+    //为什么要arraylist，因为有getRandom函数，随机化
+    ArrayList<Integer> nums;
+    HashMap<Integer, Integer> locs;
+    Random rand = new Random();
+    
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        nums = new ArrayList<Integer>();
+        locs = new HashMap<Integer, Integer>();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( contain ) return false;
+        locs.put( val, nums.size());  //hashmap中第二个参数是怎么确定的？ 放了一个index，为什么要放index？
+        nums.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( ! contain ) return false;
+        int loc = locs.get(val);
+        if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
+            int lastone = nums.get(nums.size() - 1 );
+            nums.set( loc , lastone );
+            locs.put(lastone, loc);
+        }
+        locs.remove(val);
+        nums.remove(nums.size() - 1);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return nums.get( rand.nextInt(nums.size()) );
+    }
+}
