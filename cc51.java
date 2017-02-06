37996b3d012be262040e4be4a5a779f8a5563dcb
unknown <381242448@qq.com>
2017-02-06 11:08:18 -0500

    add cc51

---
+++ cc51.java
@@ -0,0 +1,15 @@
+public static int updateBits(int n, int m, int i, int j) {
+ int max = ~0; /* All 1¡¯s */
+
+ // 1¡¯s through position j, then 0¡¯s
+ int left = max - ((1 << j) - 1);
+
+ // 1¡¯s after position i
+ int right = ((1 << i) - 1);
+
+ // 1¡¯s, with 0s between i and j
+ int mask = left | right;
+
+ // Clear i through j, then put m in there
+ return (n & mask) | (m << i);
+}
